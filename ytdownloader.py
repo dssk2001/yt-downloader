a32474e0f54ccc4c50c8b7f31b15b2462f33411c
dssk2001 <65063740+dssk2001@users.noreply.github.com>
2020-08-29 08:43:53 +0530

    Update ytdownloader.py

--- ytdownloader.py
+++ ytdownloader.py
@@ -2,5 +2,5 @@ import pytube
 video_url = input("Enter URL:") # copy and paste url
 youtube = pytube.YouTube(video_url)
 video = youtube.streams.first()
-video.download('D:\IITM')
+video.download('path/to/download')
 printf("Done Downloading")
